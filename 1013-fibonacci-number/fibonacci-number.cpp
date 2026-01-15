8fb125867e671e85468e0c2a1fa6e1b4b85dca51
dhairyadesai26 <202451051@iiitvadodara.ac.in>
2026-01-16 00:09:16 +0530

    Time: 3 ms (62.89%) | Memory: 8.1 MB (16.15%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
  int fb(int n,vector<int> &dp){
-    if(n<=1){
-        return n;
+    if(n==0) return 0;
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
     }
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-    dp[n]=fb(n-1,dp)+fb(n-2,dp);
     return dp[n];
  }
     int fib(int n) {
         vector<int> dp(n+1);
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
-        }
      return fb(n,dp);
 
     }
