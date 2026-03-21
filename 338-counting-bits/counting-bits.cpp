f1927574d30935c6f4de224be74a98cf81f5aa86
dhairyadesai26 <202451051@iiitvadodara.ac.in>
2026-03-21 14:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (20.05%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0;i<=n;i++){
-         int count = __builtin_popcount(i); 
-            ans.push_back(count);
+           ans.push_back( __builtin_popcount(i));
         }
         return ans;
     }
