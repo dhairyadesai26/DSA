f6d77230a4a33a11f150aeb2c076f0cbfc841b3e
dhairyadesai26 <202451051@iiitvadodara.ac.in>
2026-03-21 15:07:51 +0530

    Time: 24 ms (5.62%) | Memory: 15.6 MB (9.28%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
+ int solve(int i){
+    string s="";
+    while(i!=0){
+        s+=char(i%2);
+        i=i/2;
+
+    }
+
+    int n=s.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='\1'){
+            count++;
+        }
+    }
+    return count;
+ }
     vector<int> countBits(int n) {
         vector<int> ans;
         for(int i=0;i<=n;i++){
-           ans.push_back( __builtin_popcount(i));
+           ans.push_back(solve(i));
         }
         return ans;
     }
