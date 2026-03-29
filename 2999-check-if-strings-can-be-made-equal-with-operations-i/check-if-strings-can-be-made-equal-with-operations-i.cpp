a7cdbfe0503f73792512ada34632a6b0e4111cd7
dhairyadesai26 <202451051@iiitvadodara.ac.in>
2026-03-29 11:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (67.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        int m=s1.size();
-        int n=s2.size();
+        //check for pre-match
         if(s1==s2){
             return true;
         }
-        for(int i=0;i<m-1;i++){
-            for(int j=i+1;j<m;j++){
+        // check that if s1 can be made equal to s2
+        for(int i=0;i<s1.size()-1;i++){
+            for(int j=i+1;j<s1.size();j++){
                  if(j-i==2){
                     swap(s1[i],s1[j]);
                  }
                  if(s1==s2) return true;
             }
         }
-          for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
+            // check that if s2 can be made equal to s1
+          for(int i=0;i<s2.size()-1;i++){
+            for(int j=i+1;j<s2.size();j++){
                  if(j-i==2){
                     swap(s2[i],s2[j]);
                  }
                  if(s1==s2) return true;
             }
         }
+        // cannot be made equal
         return false;
     }
 };
