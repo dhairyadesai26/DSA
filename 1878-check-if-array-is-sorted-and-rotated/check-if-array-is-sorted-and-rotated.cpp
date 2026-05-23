25f397015382f5c24c89cee533b0616cce1758e1
dhairyadesai26 <202451051@iiitvadodara.ac.in>
2026-05-23 13:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (82.15%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,36 +2,13 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
     int n = nums.size();
-
-    vector<int> nums1 = nums;
-    sort(nums1.begin(), nums1.end());
-
-    int a = -1;
-
-    // Find rotation amount
-    for(int x = 0; x < n; x++) {
-
-        bool found = true;
-
-        for(int i = 0; i < n; i++) {
-
-            if(nums[(i + x) % n] != nums1[i]) {
-                found = false;
-                break;
+    int count=0;
+    for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i + 1) % n]) {
+            count++;
             }
         }
 
-        if(found) {
-            a = x;
-            break;
-        }
-    }
-
-    // If no valid rotation found
-    if(a == -1) {
-        return false;
-    }
-
-    return true;
+    return count<=1;
 }
 };
