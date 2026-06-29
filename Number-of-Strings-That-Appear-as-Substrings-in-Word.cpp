778002008749eae78dc2c72d75bb2ff8e4dfc2b5
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-06-29 12:17:04 +0530

    [1967] [Time Beats: 100.00%] [Memory Beats: 61.65%] - LeetPush

--- Number-of-Strings-That-Appear-as-Substrings-in-Word.cpp
+++ Number-of-Strings-That-Appear-as-Substrings-in-Word.cpp
@@ -1,22 +1,12 @@
 1class Solution {
 2public:
 3    int numOfStrings(vector<string>& patterns, string word) {
-4        int n=word.length();
-5        int count=0;
-6        vector<string> substrings;
-7        for(int i=0;i<n;i++){
-8            for(int j=i;j<n;j++){
-9                substrings.push_back(word.substr(i, j - i + 1));
-10
-11            }
-12
-13        }
-14         for(string s:patterns){
-15           auto it=find(substrings.begin(),substrings.end(),s);
-16           if(it!=substrings.end()){
-17            count++;
-18           }
-19         }
-20        return count;
-21    }
-22};
+4        int count=0;
+5        for(string &s:patterns){
+6            if(word.find(s)!=string::npos){
+7                count++;
+8            }
+9        }
+10        return count;
+11    }
+12};
