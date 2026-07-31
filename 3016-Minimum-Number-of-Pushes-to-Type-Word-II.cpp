a31c9bf3d734fd7e5e2ca93b6e4eec1b381793fb
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-07-31 12:43:29 +0530

    [3016] [Time Beats: 69.76%] [Memory Beats: 25.39%] - LeetPush

---
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> mp(26,0);
+        for(char &ch:word){
+            mp[ch-'a']++;
+        }
+        sort(mp.rbegin(),mp.rend());
+        int result=0;
+        for(int i=0;i<26;i++){
+            int freq=mp[i];
+            int press=i/8+1;
+            result+=press*freq;
+        }
+        return result;
+
+
+    }
+};
