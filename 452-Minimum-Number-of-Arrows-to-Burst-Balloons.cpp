a5ff03c77747ac39bf46d0ed10251e5ee6e5d319
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-08-05 21:56:32 +0530

    [452] [Time Beats: 93.02%] [Memory Beats: 26.46%] - LeetPush

---
+++ 452-Minimum-Number-of-Arrows-to-Burst-Balloons.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int n=points.size();
+        if(points.empty()) return 0;
+        sort(points.begin(),points.end(),[](vector<int>& a, vector<int>& b) {
+                 return a[1] < b[1];
+             });
+        int arrows=1;
+        int end=points[0][1];
+        for(int i=1;i<n;i++){
+            if(points[i][0]>end){
+                arrows++;
+                end=points[i][1];
+            }
+        }
+        return arrows;
+
+    }
+};
