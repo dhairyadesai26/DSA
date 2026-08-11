3062ca23912a9bec8ae7384fd85b11f0e1365f86
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-08-11 11:20:52 +0530

    [2996] [Time Beats: 100.00%] [Memory Beats: 29.32%] - LeetPush

---
+++ 2996-Smallest-Missing-Integer-Greater-Than-Sequential-Prefix-Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int n=nums.size();
+        int sum=nums[0];
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]==nums[i]+1){
+                sum+=nums[i+1];
+            }
+            else{
+                break;
+            }
+
+        }
+        unordered_set<int> st(nums.begin(),nums.end());
+      
+        while(st.count(sum)){
+            sum++;
+        }
+        return sum;
+
+    }
+};
