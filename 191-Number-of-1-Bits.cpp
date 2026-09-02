b5a57f4f1c5888c1e9550870aa347935427a7c14
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-09-03 01:48:13 +0530

    [191] [Time Beats: 100.00%] [Memory Beats: 11.15%] - LeetPush

--- 191-Number-of-1-Bits.cpp
+++ 191-Number-of-1-Bits.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     int hammingWeight(int n) {
     int count=0;
-    string x="";
     while(n!=0){
-        char temp=(n % 2) +'0';
-        x+=temp;
+        if(n % 2==1) count++;
         n/=2;
-    }
-    for(int i=0;i<x.length();i++){
-        if(x[i]=='1'){
-            count++;
-        }
     }
     return count;
 
