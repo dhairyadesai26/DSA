fd7d6248d5c21724de12e1240e95c461d68caa00
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-09-03 11:46:36 +0530

    [3876] [Time Beats: 72.51%] [Memory Beats: 87.43%] - LeetPush

---
+++ 3876-Construct-Uniform-Parity-Array-II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+        bool OddFlag=true;
+        bool EvenFlag=true;
+        for(int i=0;i<n;i++){
+            if(nums1[i] % 2==0) OddFlag=false;
+            if(nums1[i] % 2!=0) EvenFlag=false;
+        }
+        if(OddFlag || EvenFlag) return true;
+        int mini=INT_MAX;
+        for(int i=0;i<nums1.size();i++){
+            mini=min(mini,nums1[i]);
+        }
+        if(mini %2 ==0) return false;
+        return true;
+
+
+        
+        
+    }
+};
