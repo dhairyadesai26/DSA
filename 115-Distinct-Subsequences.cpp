9f51cd0df0d1afe84a0ea33bed019c92175bea25
DHAIRYA DESAI <202451051@iiitvadodara.ac.in>
2026-09-06 15:01:15 +0530

    [115] [Time Beats: 39.07%] [Memory Beats: 11.48%] - LeetPush

--- 115-Distinct-Subsequences.cpp
+++ 115-Distinct-Subsequences.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int m,n;
-    int solve(int i,int j,string& s,string& t){
-        if(j==n) return 1;
-        if(i==m) return 0;
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return dp[i][j]= solve(i+1,j+1,s,t) + solve(i+1,j,s,t);
-        }
-        else{
-            return dp[i][j]=solve(i+1,j,s,t);
-        }
-        
-
-
-
-
-    }
+typedef unsigned long long ull;
     int numDistinct(string s, string t) {
-        m=s.length();
-        n=t.length();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,s,t);
+        int m=s.length();
+        int n=t.length();
+        vector<vector<ull>> dp(m+1,vector<ull>(n+1));
+        for(int i=0;i<=m;i++){
+            dp[i][0]=1;
 
-    
-        
+        }
+        for(int i=1;i<=n;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                 if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                 }
+                 else{
+                    dp[i][j]=dp[i-1][j];
+                 }
+            }
+        }
+        return dp[m][n];
+
     }
 };
